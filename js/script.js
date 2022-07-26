7f5204a192fb9682599b33a4597f8cd5706f269b
SeeGrey <coddign@gmail.com>
2022-07-26 21:34:56 +0300

    Home2

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@
 
 const numberOfFilms = +prompt('Сколько фильмов вы смотрели', '');
 
-let filmName = prompt("Один из последних просмотренных фильмов", '');
-let filmGrade = +prompt("На сколько оцениваете его?", '');
-
 
 let personalMovieDB = {
     count : numberOfFilms,
@@ -14,8 +11,29 @@ let personalMovieDB = {
     privat : false
 };
 
-personalMovieDB.movies[filmName] = filmGrade;
+for (let i = 0; i < 2; i++) {
 
+    let a = prompt("Один из последних просмотренных фильмов", '');
+    let b = +prompt("На сколько оцениваете его?", '');
+    
+        
+    if(a == null || a.trim() == "" || a.length >= 50 || b == 0 || b.length == 0 || Number.isNaN(b)){
+        i--;
+    } else {
+        personalMovieDB.movies[a] = b;
+    }
+
+}
+
+if(personalMovieDB.count < 10){
+    alert('Maло фильмoв');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    alert('Классный зритель');
+} else if(personalMovieDB.count >= 30){
+    alert('Киноман');
+} else{
+    alert('Ошибка');
+}
 
 
 console.log(personalMovieDB);
