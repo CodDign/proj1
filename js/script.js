941386ef2bf04222b79f6c23ae0db8d031cf368c
SeeGrey <coddign@gmail.com>
2022-07-26 16:36:17 +0300

    home1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 "use strict";
 
-let number = 'hello';
+const numberOfFilms = +prompt('Сколько фильмов вы смотрели', '');
 
-alert(number);
+let filmName = prompt("Один из последних просмотренных фильмов", '');
+let filmGrade = +prompt("На сколько оцениваете его?", '');
+
+
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+personalMovieDB.movies[filmName] = filmGrade;
+
+
+
+console.log(personalMovieDB);
+
+
+
+
+// let amount = 2;
+
+// for (let i = 0; i < amount.length; i++) {
+// }
